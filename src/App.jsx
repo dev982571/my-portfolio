76e2df730a622fc5405ef76b8c49f8dfda8895b8
dev982571 <dev982571@gmail.com>
2026-09-22 10:19:55 +0530

    fix import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Slider from "./components/Slider";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import Slider from "./components/slider";
 
 function App() {
   return (
